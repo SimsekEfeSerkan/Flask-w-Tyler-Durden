2828b5368d24f5eac31961acb580c964f67076dc
Efe  Şimşek <107925062+SimsekEfeSerkan@users.noreply.github.com>
2025-01-20 22:01:25 +0300

    Add files via upload

---
+++ gpt.py
@@ -0,0 +1,16 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
